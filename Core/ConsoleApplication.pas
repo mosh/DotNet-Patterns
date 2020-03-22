f6b179b012fb56a2f3e6514d0b9a047d52bb0fae
John Moshakis <john.moshakis@firmex.com>
2020-03-22 18:41:32 -0400

    log somewhere else

--- Core/ConsoleApplication.pas
+++ Core/ConsoleApplication.pas
@@ -2,6 +2,7 @@
 
 uses
   Microsoft.Extensions.Configuration,
+  Microsoft.Extensions.Logging,
   System.Threading.Tasks;
 
 type
@@ -9,18 +10,20 @@ type
   private
     property TestService:ITestService;
     property Configuration:IConfiguration;
+    property Logger:ILogger;
 
   public
-    constructor(testServiceImpl:ITestService; configurationImpl:IConfiguration);
+    constructor(testServiceImpl:ITestService; configurationImpl:IConfiguration; loggerImpl:ILogger<ConsoleApplication>);
     begin
       TestService := testServiceImpl;
       Configuration := configurationImpl;
+      Logger := loggerImpl;
     end;
 
     method Run:Task;
     begin
       var location := Configuration.GetValue<String>('Location');
-
+      Logger.LogInformation($'Location is {location}');
       await TestService.SomeMethod;
     end;
   end;
