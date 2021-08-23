6c30ffc05526cc9be596b03df8a45c12cdfc0cab
John Moshakis <john@moshakis.com>
2021-08-22 23:05:04 -0400

    Added worker service

---
+++ WorkerServiceConsoleApplication/Program.pas
@@ -0,0 +1,32 @@
+﻿namespace WorkerServiceConsoleApplication;
+
+uses
+  Microsoft.Extensions.Hosting,
+  System.Linq;
+
+type
+
+  // https://wakeupandcode.com/worker-service-in-net-core-3-1/
+
+  Program = class
+  public
+    class method Main(args: array of String): Int32;
+    begin
+      CreateHostBuilder(args).Build.Run;
+    end;
+
+    class method CreateHostBuilder(args:array of String):IHostBuilder;
+    begin
+      var hostBuilder := Host.CreateDefaultBuilder(args);
+
+      hostBuilder.ConfigureServices(services -> begin
+          services.AddHostedService<Worker>;
+        end);
+
+      exit hostBuilder;
+    end;
+
+  end;
+
+
+end.
---
+++ WorkerServiceConsoleApplication/Worker.pas
@@ -0,0 +1,31 @@
+﻿namespace WorkerServiceConsoleApplication;
+
+uses
+  Microsoft.Extensions.Hosting;
+
+type
+  Worker = public class(BackgroundService)
+  protected
+    method ExecuteAsync(stoppingToken: System.Threading.CancellationToken): Task; override;
+    begin
+
+
+
+      var startedTask := Task.Factory.StartNew(method begin
+
+          while not stoppingToken.IsCancellationRequested do
+          begin
+          end;
+          Console.WriteLine('Cancelled');
+
+
+        end, stoppingToken);
+      exit startedTask;
+
+
+
+    end;
+
+  end;
+
+end.
