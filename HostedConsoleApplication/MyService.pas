20d3d4eeedb4beb75fced8e80461e7adf2a502b6
John Moshakis <john@moshakis.com>
2023-07-06 15:13:06 -0400

    Added hosted example

---
+++ HostedConsoleApplication/MyService.pas
@@ -0,0 +1,23 @@
+﻿namespace HostedConsoleApplication;
+
+uses
+  Microsoft.Extensions.Hosting;
+
+type
+  MyService = public class(IHostedService)
+  private
+  protected
+  public
+
+    method StartAsync(cancellationToken: System.Threading.CancellationToken): Task;
+    begin
+      exit Task.CompletedTask;
+    end;
+
+    method StopAsync(cancellationToken: System.Threading.CancellationToken): Task;
+    begin
+      exit Task.CompletedTask;
+    end;
+  end;
+
+end.
---
+++ HostedConsoleApplication/Program.pas
@@ -0,0 +1,25 @@
+﻿namespace HostedConsoleApplication;
+
+
+uses
+  Microsoft.Extensions.Hosting,
+  System.Linq,
+  System.Threading.Tasks;
+
+type
+  Program = class
+  private
+
+    class method Main(args: array of String): Task;
+    begin
+      await Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) ->
+          begin
+            services.AddHostedService<MyService>();
+          end)
+        .RunConsoleAsync;
+    end;
+
+  end;
+
+end.
